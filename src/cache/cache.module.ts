cab5299716fdc816c8eaf0178b45326217edc6bf
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-07-15 22:54:50 +0900

    add cache

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,4 +1,23 @@
-import { Module } from '@nestjs/common'
+import { CacheModule as CModule, Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ClientOptions } from '@nestjs/microservices'
+import * as redisStore from 'cache-manager-ioredis'
+import { CacheService } from './cache.service'
 
-@Module({})
+@Module({
+  imports: [
+    CModule.registerAsync<ClientOptions>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        store: redisStore,
+        host: configService.get('REDIS_HOST'),
+        port: 6379,
+        db: 1,
+        ttl: 3600 * 48
+      })
+    })
+  ],
+  providers: [CacheService]
+})
 export class CacheModule {}
--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@nestjs/common'
+import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common'
+import { Cache } from 'cache-manager'
 
 @Injectable()
-export class CacheService {}
+export class CacheService {
+  constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
+}
