ebb7ef98aecd72428a7d3c39cb039ad76778331b
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-07-16 17:24:36 +0900

    delete jwt config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,25 @@ import { BasicStrategy } from './strategies/basic.strategy'
 import { AuthService } from './auth.service'
 import { JwtStrategy } from './strategies/jwt.strategy'
 import { JwtModule } from '@nestjs/jwt'
-import { JwtConfig } from '../config/jwt.config'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { UserRepository } from '../models/mysql/user/user.repository'
 import { CacheModule } from '../cache/cache.module'
+import { PassportModule } from '@nestjs/passport'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
-    JwtModule.registerAsync(JwtConfig),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '60s' }
+      })
+    }),
     TypeOrmModule.forFeature([UserRepository], 'MySQL'),
-    CacheModule
+    CacheModule,
+    PassportModule
   ],
   providers: [BasicStrategy, JwtStrategy, AuthService]
 })
