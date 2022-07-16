2a0f3f24a9b2b0c61d6a0044f570553b845c7db1
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-07-16 17:02:02 +0900

    fix(test): article.service.spec.ts - 세팅 완료!!!!

--- src/api/articles/article.service.spec.ts
+++ src/api/articles/article.service.spec.ts
@@ -1,10 +1,5 @@
-import { getConnectionToken, getModelToken } from '@nestjs/mongoose'
 import { Test, TestingModule } from '@nestjs/testing'
-import { getRepositoryToken } from '@nestjs/typeorm'
-import { Connection } from 'mongoose'
 import { ArticleModel } from '../../models/mongo/article/article.model'
-import { ArticleModelModule } from '../../models/mongo/article/article.model.module'
-import { Article } from '../../models/mongo/article/article.schema'
 import { Mock } from '../../types/test.type'
 import { ArticleService } from './article.service'
 
@@ -16,22 +11,17 @@ describe('ArticleService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [ArticleModelModule],
       providers: [
         ArticleService,
         {
           provide: ArticleModel,
-          inject: [getConnectionToken('mongo')],
-          useFactory: (connection: Connection) => ({
-            provide: getModelToken(Article.name, 'mongo'),
-            useFactory: () => mockArticleModel()
-          })
+          useValue: mockArticleModel()
         }
       ]
     }).compile()
 
     service = module.get<ArticleService>(ArticleService)
-    articleModel = module.get(getModelToken(Article.name, 'mongo'))
+    articleModel = module.get(ArticleModel)
   })
 
   it('should be defined', () => {
