f6c44eb4a14fa071e478b6a3d4ca17f322cb90be
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-07-16 17:08:45 +0900

    delete rmq config

--- src/rabbitmq/rabbitmq.module.ts
+++ src/rabbitmq/rabbitmq.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common'
-import { ClientsModule } from '@nestjs/microservices'
-import { RabbitMQConfig } from './rabbitmq.config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ClientsModule, Transport } from '@nestjs/microservices'
 import { RabbitmqService } from './rabbitmq.service'
 
 @Module({
-  imports: [ClientsModule.registerAsync([RabbitMQConfig])],
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'RabbitMQ',
+        inject: [ConfigService],
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue: 'queue_name',
+            queueOptions: {}
+          }
+        })
+      }
+    ])
+  ],
   providers: [RabbitmqService]
 })
 export class RabbitmqModule {}
