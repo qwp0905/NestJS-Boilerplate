b2fa86ce4734d032985e509ac6bc2a8f8ffc607a
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-07-17 23:42:57 +0900

    rename decorators

--- src/decorators/api.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { applyDecorators, Get } from '@nestjs/common'
-import { R } from './response.decorator'
-
-export const GET = (path?: string): MethodDecorator => {
-  return applyDecorators(R(), Get(path))
-}
--- src/decorators/http.decorator.ts
+++ src/decorators/http.decorator.ts
@@ -1,3 +1,9 @@
+import { applyDecorators, Get } from '@nestjs/common'
+
+export const GET = (path?: string): MethodDecorator => {
+  return applyDecorators(R(), Get(path))
+}
+
 export const R = (): MethodDecorator => {
   return (target: any, property: string, descriptor: PropertyDescriptor) => {
     const method = descriptor.value
