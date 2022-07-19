16519c161ed63262e54899cfcb13e42adb6ab794
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-07-19 21:56:17 +0900

    fix vscode setting

--- src/databasse/typeorm.config.ts
+++ src/databasse/typeorm.config.ts
@@ -1,11 +1,14 @@
 import { ConfigModule, ConfigService } from '@nestjs/config'
-import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm'
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions
+} from '@nestjs/typeorm'
 
 export const typeORMConfig: TypeOrmModuleAsyncOptions = {
   name: 'MySQL',
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: (configService: ConfigService) => ({
+  useFactory: (configService: ConfigService): TypeOrmModuleOptions => ({
     type: 'mysql',
     host: configService.get<string>('DATABASE_HOST'),
     port: configService.get<number>('DATABASE_PORT'),
