1256d733b649ae1157047a6606ae78a58f8e7929
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-08-17 20:54:08 +0900

    fix event

---
+++ src/event/event.register.service.ts
@@ -0,0 +1,25 @@
+import { IMongoDB, ISignature } from '@interfaces'
+import { Injectable } from '@nestjs/common'
+import { EventEmitter2 } from '@nestjs/event-emitter'
+import { FilterQuery } from 'mongoose'
+import { SignatureEvent } from '@event'
+
+@Injectable()
+export class EventRegisterService {
+  constructor(private readonly eventEmitter: EventEmitter2) {}
+
+  testEvent<T extends keyof IMongoDB>(
+    collection: T,
+    filter_query: FilterQuery<IMongoDB[T]>,
+    previous: IMongoDB[T],
+    signature: ISignature
+  ) {
+    const event = new SignatureEvent<IMongoDB, T>()
+    event.collection = collection
+    event.filter_query = filter_query
+    event.previous = previous
+    event.signature = signature
+
+    return this.eventEmitter.emit('test', event)
+  }
+}
