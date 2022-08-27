9a610e9f83ab6e4bd1d26a9e74f7056711a7e3e2
qwp0905 <91672778+qwp0905@users.noreply.github.com>
2022-08-27 17:48:37 +0900

    fix memory server to e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,31 +4,39 @@ import * as request from 'supertest'
 import { AppModule } from '@app'
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import * as mongoose from 'mongoose'
+import { DataSource } from 'typeorm'
+import { MongooseConfig, typeORMConfig } from '@database'
+
+jest.mock('./../src/external/database/mongoose.config')
+jest.mock('./../src/external/database/typeorm.config')
+const mockMConf = jest.mocked(MongooseConfig)
+const mockTConf = jest.mocked(typeORMConfig)
 
 describe('AppController (e2e)', () => {
   let app: INestApplication
   let mongod: MongoMemoryServer
+  let mysql: DataSource
 
   beforeAll(() => {
-    jest.mock('./../src/external/database/mongoose.config', () => ({
-      connectionName: 'mongo',
-      async useFactory() {
-        mongod = await MongoMemoryServer.create()
-        return {
-          uri: mongod.getUri()
-        }
+    mockMConf.connectionName = 'mongo'
+    mockMConf.useFactory.mockImplementation(async () => {
+      mongod = await MongoMemoryServer.create()
+      return {
+        uri: mongod.getUri()
       }
-    }))
+    })
 
-    jest.mock('./../src/external/database/typeorm.config', () => ({
-      name: 'MySQL',
-      useFactory: () => ({
+    mockTConf.name = 'MySQL'
+    mockTConf.useFactory.mockImplementation(() => {
+      mysql = new DataSource({
         type: 'sqlite',
         database: ':memory:',
         entities: ['dist/src/models/mysql/**/*.entity.js'],
-        synchronize: false
+        synchronize: true,
+        dropSchema: true
       })
-    }))
+      return mysql.options
+    })
   })
 
   beforeEach(async () => {
